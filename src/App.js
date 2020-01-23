a3cfc2b1a91282e128eb8af6adb3401425c1aba4
nixdie <nixdie@yandex.ru>
2020-01-23 23:04:05 +0700

    added searchBar and Logs components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { Fragment, useEffect } from 'react';
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
+
 
 import './App.css';
 
@@ -11,9 +14,12 @@ function App() {
     M.AutoInit();
   })
   return (
-    <div>
-      app <i className="material-icons">adb</i>
-    </div>
+    <Fragment>
+      <SearchBar />
+       <div className="container">
+         <Logs />
+       </div>
+    </Fragment>
   );
 }
 
