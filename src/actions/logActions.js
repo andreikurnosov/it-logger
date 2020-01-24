bfb2fa25a4c2a23223466a64f291170e01e7024f
nixdie <nixdie@yandex.ru>
2020-01-24 15:09:32 +0700

    deletes logs from db action

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,4 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from './types';
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, DELETE_LOG } from './types';
 
 export const getLogs = () => async (dispatch) => {
   try {
@@ -42,6 +42,25 @@ export const addLog = (log) => async (dispatch) => {
   }
 }
 
+
+export const deleteLog = (id) => async (dispatch) => {
+  try {
+    setLoading();
+    const res = await fetch(`/logs/${id}`, {
+      method: 'DELETE'
+    });
+
+    dispatch({
+      type: DELETE_LOG,
+      payload: id
+    });
+  } catch(err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data
+    })
+  }
+}
 
 // set loading to true
 export const setLoading = () => {
--- src/components/logs/LogItem.jsx
+++ src/components/logs/LogItem.jsx
@@ -1,20 +1,28 @@
 import React from 'react'
 import Moment from 'react-moment';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types'
+import { deleteLog } from '../../actions/logActions';
+import M from 'materialize-css/dist/js/materialize.min.js';
 
-const LogItem = ({ log }) => {
+const LogItem = ({ log, deleteLog }) => {
+  const { attention, tech, message, id, date } = log;
+  const onDelete = () => {
+    deleteLog(id);
+    M.toast({ html: 'Log Deleted' });
+  }
   return (
     <li className="collection-item">
       <div>
-        <a href="#edit-log-modal" className={`modal-trigger ${log.attention ? 'red-text' : 'blue-text'}`}>{log.message}</a>
-        <br/>
+        <a href="#edit-log-modal" className={`modal-trigger ${attention ? 'red-text' : 'blue-text'}`}>{message}</a>
+        <br />
         <span className="grey-text">
-          <span className="black-text">ID #{log.id}</span>{' '}
+          <span className="black-text">ID #{id}</span>{' '}
           last updated by {' '}
-          <span className="black-text">{log.tech}</span>
-          {' '} on <Moment format="MMMM Do YYYY, h:mm:ss a">{log.date}</Moment>
+          <span className="black-text">{tech}</span>
+          {' '} on <Moment format="MMMM Do YYYY, h:mm:ss a">{date}</Moment>
         </span>
-        <a href="#!" className="secondary-content">
+        <a href="#!" className="secondary-content" onClick={onDelete}>
           <i className="material-icons grey-text">delete</i>
         </a>
       </div>
@@ -23,7 +31,14 @@ const LogItem = ({ log }) => {
 }
 
 LogItem.propTypes = {
-  log: PropTypes.object.isRequired
+  log: PropTypes.object.isRequired,
+  deleteLog: PropTypes.func.isRequired
 }
 
-export default LogItem
+const mapDispatchToProps = dispatch => {
+  return {
+    deleteLog: (id) => dispatch(deleteLog(id))
+  }
+}
+
+export default connect(null, mapDispatchToProps)(LogItem);
