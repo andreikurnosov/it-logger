d3fdb2f0bcaddefe41d9a4e1aef4ca4815af9d2f
nixdie <nixdie@yandex.ru>
2020-01-24 08:02:13 +0700

    modal and button for adding logs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Fragment, useEffect } from 'react';
 import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
+import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
-
 
 import './App.css';
 
@@ -17,6 +18,8 @@ function App() {
     <Fragment>
       <SearchBar />
        <div className="container">
+         <AddBtn />
+         <AddLogModal />
          <Logs />
        </div>
     </Fragment>
